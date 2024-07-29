d3b52cfb8ab6966baa147b7f3e9a511aad608923
Evan Laurans <evan.laurans@nexelec.fr>
2024-07-29 17:26:10 +0200

    header of service_template included

--- lbm_lib/smtc_modem_core/modem_services/service_template/service_template.c
+++ lbm_lib/smtc_modem_core/modem_services/service_template/service_template.c
@@ -39,7 +39,7 @@
 
 #include <stdint.h>   // C99 types
 #include <stdbool.h>  // bool type
-#include "lorawan_template.h"
+#include "service_template.h"
 #include "modem_core.h"
 #include "modem_supervisor_light.h"
 #include "smtc_modem_api.h"
